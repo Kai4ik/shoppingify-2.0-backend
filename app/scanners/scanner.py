77c331948a41df3d458ea405383adb7de9979452
Kai <orozobekov.kai@gmail.com>
2023-04-11 03:34:23 -0400

    refactor: activating veryfi code

--- app/scanners/scanner.py
+++ app/scanners/scanner.py
@@ -3,10 +3,9 @@ import importlib.resources
 import json
 from fastapi import UploadFile
 from fuzzywuzzy import fuzz
-# import requests
-# import os
-# import veryfi
-# from veryfi.errors import BadRequest
+import os
+import veryfi
+from veryfi.errors import BadRequest
 
 # internal modules
 from parsers import Parser
@@ -19,7 +18,7 @@ class Scanner:
         self.file = file
 
     def scan_via_veryfi(self, file: UploadFile):
-        '''
+
         client_id = os.getenv("veryfi_client_id")
         client_secret = os.getenv("veryfi_client_secret")
         username = os.getenv("veryfi_username")
@@ -28,30 +27,28 @@ class Scanner:
         client = veryfi.Client(client_id, client_secret, username, api_key)
         try:
             data = client.process_document(file)
-            // TODO: 48-63 go here
+            scanned_merchant = data["vendor"]["name"]
+            merchant = self.merchant_matching(scanned_merchant)
+            self.result["merchant"] = merchant
+            self.result["merchant_address"] = data["vendor"]["address"].replace("\n", " ")
+            self.result["tax"] = float(data["tax"]) if data["tax"] else 0
+            self.result["subtotal"] = float(data["subtotal"])
+            self.result["total"] = float(data["total"])
+            self.result["currency"] = data["currency_code"] if data["currency_code"] else "CAD"
+            self.result["purchase_date"] = data["date"]
+            self.result["payment_type"] = data["payment"]["type"]
+            self.result["receipt_number"] = data["invoice_number"]
+
+            ocr_text = data["ocr_text"]
+            parser = Parser("veryfi", ocr_text, merchant)
+            self.result["line_items"] = parser.fetch_data()
+            self.success = True
         except BadRequest as err:
             self.result["error_message"] = err.split(",")[-1].strip()
-        '''
 
-        with importlib.resources.open_text("scanners", "data.json") as json_file:
-            data = json.load(json_file)
+        # with importlib.resources.open_text("scanners", "data8.json") as json_file:
+            # data = json.load(json_file)
 
-        scanned_merchant = data["vendor"]["name"]
-        merchant = self.merchant_matching(scanned_merchant)
-        self.result["merchant"] = merchant
-        self.result["merchant_address"] = data["vendor"]["address"].replace("\n", " ")
-        self.result["tax"] = float(data["tax"]) if data["tax"] else 0
-        self.result["subtotal"] = float(data["subtotal"])
-        self.result["total"] = float(data["total"])
-        self.result["currency"] = data["currency_code"] if data["currency_code"] else "CAD"
-        self.result["purchase_date"] = data["date"]
-        self.result["payment_type"] = data["payment"]["type"]
-        self.result["receipt_number"] = data["invoice_number"]
-
-        ocr_text = data["ocr_text"]
-        parser = Parser("veryfi", ocr_text, merchant)
-        self.result["line_items"] = parser.fetch_data()
-        self.success = True
         return self.result, self.success
 
     @staticmethod
@@ -59,8 +56,8 @@ class Scanner:
         merchant: str = ''
         loblaws_match = fuzz.ratio(scanned_merchant.lower(), "loblaws")
         shoppers_match = fuzz.ratio(scanned_merchant.lower(), "shoppers drug mart")
-        if loblaws_match > 70 or "loblaws" in scanned_merchant.lower():
+        if loblaws_match > 60 or "loblaws" in scanned_merchant.lower():
             merchant = "Loblaws"
-        if shoppers_match > 70 or "shoppers drug mart" in scanned_merchant.lower():
+        if shoppers_match > 60 or "shoppers drug mart" in scanned_merchant.lower():
             merchant = "Shoppers Drug Mart"
         return merchant if merchant != '' else scanned_merchant
